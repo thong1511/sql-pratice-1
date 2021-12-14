497004e91958a236dcf7cd085bfc958afb2f7b8b
Thông Nguyễn <66173775+thong1511@users.noreply.github.com>
2021-12-14 17:47:49 +0700

    Update sql_practice_1.sql

--- sql_practice_1.sql
+++ sql_practice_1.sql
@@ -1,15 +1,15 @@
 --- create 1st table
 CREATE TABLE sales_overview (
-	product_id INT PRIMARY KEY,
-	products VARCHAR(30),
-	sales INT
+	product_id INT PRIMARY KEY
+	,products VARCHAR(30)
+	,sales INT
 );
 
 --- create 2nd table
 CREATE TABLE product_perf (
-	months VARCHAR(15),
-	title VARCHAR(40),
-	product_id INT FOREIGN KEY REFERENCES sales_overview(product_id)
+	months VARCHAR(15)
+	,title VARCHAR(40)
+	,product_id INT FOREIGN KEY REFERENCES sales_overview(product_id)
 );
 
 --- insert records into 1st table
@@ -46,28 +46,28 @@ INSERT INTO product_perf VALUES('April','Worst-seller',11);
 --- Q1:
 --- What is the highest sales by each product?
 SELECT 
-	products,
-	MAX(sales) AS bestsellers 
+	products
+	,MAX(sales) AS bestsellers 
 FROM sales_overview
 GROUP BY products;
 
 --- Q2:
 --- Of all 3 best-sellers in the 2nd table, how many times did each of them get the title in April?
 SELECT 
-     product_perf.product_id,
-     c.products,
-     COUNT(*) AS num_of_tits
+     product_perf.product_id
+     ,c.products
+     ,COUNT(*) AS num_of_tits
 FROM product_perf
 JOIN (
 	SELECT 
-		a.product_id,
-		a.products,
-		a.sales
+		a.product_id
+		,a.products
+		,a.sales
 	FROM sales_overview a
 	JOIN (
 		SELECT 
-			products, 
-			MAX(sales) AS max_sales
+			products
+			,MAX(sales) AS max_sales
 		FROM sales_overview
 		GROUP BY products
 	) b
